68e5ec8fdcb2ed7b09f0f43b93a778d35c4ad311
wangrl2016 <wangrl2016@gmail.com>
2023-01-02 14:49:47 +0800

    Audio bus create unit test

--- media/base/audio_bus_test.cc
+++ media/base/audio_bus_test.cc
@@ -39,6 +39,42 @@ namespace media {
             }
         }
 
+        // Verify values for each channel in |result| are within |epsilon| of
+        // |expected|. If |epsilon| exactly equals 0. uses FLOAT_EQ macro.
+        void VerifyAreEqualWithEpsilon(const AudioBus* result,
+                                       const AudioBus* expected,
+                                       float epsilon) {
+            ASSERT_EQ(expected->channels(), result->channels());
+            ASSERT_EQ(expected->frames(), result->frames());
+            ASSERT_EQ(expected->is_bitstram_format(), result->is_bitstram_format());
+
+            if (expected->is_bitstram_format()) {
+                ASSERT_EQ(expected->GetBitstreamDataSize(),
+                          result->GetBitstreamDataSize());
+                ASSERT_EQ(expected->GetBitstreamFrames(),
+                          result->GetBitstreamFrames());
+                ASSERT_EQ(0, memcmp(expected->channel(0), result->channel(0),
+                                    result->GetBitstreamDataSize()));
+                return;
+            }
+
+            for (int ch = 0; ch < result->channels(); ch++) {
+                for (int i = 0; i < result->frames(); i++) {
+                    if (epsilon == 0) {
+                        ASSERT_FLOAT_EQ(expected->channel(ch)[i], result->channel(ch)[i]);
+                    } else {
+                        ASSERT_NEAR(expected->channel(ch)[i], result->channel(ch)[i],
+                                    epsilon);
+                    }
+                }
+            }
+        }
+
+        // Verify values for each channel in |result| against |expected|.
+        void VerifyAreEqual(const AudioBus* result, const AudioBus* expected) {
+            VerifyAreEqualWithEpsilon(result, expected, 0);
+        }
+
         // Read and write to the full extent of the allocated channel data. Also test
         // the Zero() method and verify it does as advertised. Also test data if data
         // is 16-byte aligned as advertised (see kChannelAlignment in audio_bus.h).
@@ -63,6 +99,24 @@ namespace media {
             }
         }
 
+        // Verify copying to and from |bus1| and |bus2|.
+        void CopyTest(AudioBus* bus1, AudioBus* bus2) {
+            // Fill |bus1| with dummy data.
+            for (int i = 0; i < bus1->channels(); i++) {
+                std::fill(bus1->channel(i), bus1->channel(i) + bus1->frames(), i);
+            }
+
+            // Verify copy from |bus1| to |bus2|.
+            bus2->Zero();
+            bus1->CopyTo(bus2);
+            VerifyAreEqual(bus1, bus2);
+
+            // Verify copy from |bus2| to |bus1|.
+            bus1->Zero();
+            bus2->CopyTo(bus1);
+            VerifyAreEqual(bus2, bus1);
+        }
+
     protected:
         std::vector<float*> data_;
     };
@@ -85,6 +139,7 @@ namespace media {
         VerifyReadWriteAndAlignment(bus.get());
     }
 
+    // Verify an AudioBus created via CreateWrapper(...) works as advertied.
     TEST_F(AudioBusTest, CreateWrapper) {
         // Test里面的数据结构
         data_.reserve(kChannels);
@@ -115,6 +170,46 @@ namespace media {
         EXPECT_TRUE(deleted);
     }
 
+    // Verify an AudioBus create via wrapping a vector works as advertised.
+    TEST_F(AudioBusTest, WrapVector) {
+        data_.reserve(kChannels);
+        for (int i = 0; i < kChannels; i++) {
+            data_.push_back(static_cast<float*>(base::AlignedAlloc(
+                    sizeof(*data_[i]) * kFrameCount, AudioBus::kChannelAlignment)));
+        }
+
+        std::unique_ptr<AudioBus> bus = AudioBus::WrapVector(kFrameCount, data_);
+        VerifyChannelAndFrameCount(bus.get());
+        VerifyReadWriteAndAlignment(bus.get());
+    }
+
+    TEST_F(AudioBusTest, WrapMemory) {
+        AudioParameters params(AudioParameters::AUDIO_PCM_LINEAR,
+                               ChannelLayoutConfig::FromLayout<kChannelLayout>(),
+                               kSampleRate,
+                               kFrameCount);
+        int data_size = AudioBus::CalculateMemorySize(params);
+        std::unique_ptr<float, base::AlignedFreeDeleter> data(
+                static_cast<float*>(
+                        base::AlignedAlloc(data_size,
+                                           AudioBus::kChannelAlignment)));
+
+        // Fill the memory with a test value we can check for after wrapping.
+        static const float kTestValue = 3;
+        std::fill(
+                data.get(), data.get() + data_size / sizeof(*data.get()), kTestValue);
+
+        std::unique_ptr<AudioBus> bus = AudioBus::WrapMemory(params, data.get());
+        // Verify the test value we filled prior to wrapping.
+        for (int i = 0; i < bus->channels(); i++) {
+            VerifyArrayIsFilledWithValue(bus->channel(i), bus->frames(), kTestValue);
+        }
+        VerifyChannelAndFrameCount(bus.get());
+        VerifyReadWriteAndAlignment(bus.get());
+
+        // Verify the channel vectors lie within the provided memory block.
+        EXPECT_GE(bus->channel(0), data.get());
+        EXPECT_LT(bus->channel(bus->channels() - 1) + bus->frames(),
+                  data.get() + data_size / sizeof(*data.get()));
+    }
 }
-
-
