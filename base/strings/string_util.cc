b1532f3d1deb985392e0cef06df8099c55f8b38a
wangrl2016 <wangrl2016@gmail.com>
2023-01-07 16:18:41 +0800

    String util

--- base/strings/string_util.cc
+++ base/strings/string_util.cc
@@ -70,6 +70,88 @@ namespace base {
         return !(all_char_bits & non_ascii_bit_mask);
     }
 
+    template<typename T, typename CharT = typename T::value_type>
+    std::basic_string<CharT> ToLowerASCIIImpl(T str) {
+        std::basic_string<CharT> ret;
+        ret.reserve(str.size());
+        for (size_t i = 0; i < str.size(); i++) {
+            ret.push_back(ToLowerASCII(str[i]));
+        }
+        return ret;
+    }
+
+    template<typename T, typename CharT = typename T::value_type>
+    std::basic_string<CharT> ToUpperASCIIImpl(T str) {
+        std::basic_string<CharT> ret;
+        ret.reserve(str.size());
+        for (size_t i = 0; i < str.size(); i++) {
+            ret.push_back(ToUpperASCII(str[i]));
+        }
+        return ret;
+    }
+
+    std::string ToLowerASCII(StringPiece str) {
+        return ToLowerASCIIImpl(str);
+    }
+
+    std::u16string ToLowerASCII(StringPiece16 str) {
+        return ToLowerASCIIImpl(str);
+    }
+
+    std::string ToUpperASCII(StringPiece str) {
+        return ToUpperASCIIImpl(str);
+    }
+
+    std::u16string ToUpperASCII(StringPiece16 str) {
+        return ToUpperASCIIImpl(str);
+    }
+
+#define WHITESPACE_ASCII_NO_CR_LF \
+        0x09,       /* CHARACTER TABULATION */ \
+        0x0B,       /* LINE TABULATION */      \
+        0x0C,       /* FROM FEED (FF) */       \
+        0x20        /* SPACE */
+
+#define WHITESPACE_ASCII \
+        WHITESPACE_ASCII_NO_CR_LF,  /* Comment to make clang-format linebreak */ \
+        0x0A,                       /* LINE FEED (LF) */                         \
+        0x0D                        /* CARRIAGE RETURN (CR) */
+
+#define WHITESPACE_UNICODE_NON_ASCII            \
+        0x0085, /* NEXT LINE (NEL) */           \
+        0x00A0, /* NO-BREAK SPACE */            \
+        0x1680, /* OGHAM SPACE MARK */          \
+        0x2000, /* EN QUAD */                   \
+        0x2001, /* EM QUAD */                   \
+        0x2002, /* EN SPACE */                  \
+        0x2003, /* EM SPACE */                  \
+        0x2004, /* THREE-PER-EM SPACE */        \
+        0x2005, /* FOUR-PER-EM SPACE */         \
+        0x2006, /* SIX-PER-EM SPACE */          \
+        0x2007, /* FIGURE SPACE */              \
+        0x2008, /* PUNCTUATION SPACE */         \
+        0x2009, /* THIN SPACE */                \
+        0x200A, /* HAIR SPACE */                \
+        0x2028, /* LINE SEPARATOR */            \
+        0x2029, /* PARAGRAPH SEPARATOR */       \
+        0x202F, /* NARROW NO-BREAK SPACE */     \
+        0x205F, /* MEDIUM MATHEMATICAL SPACE */ \
+        0x3000  /* IDEOGRAPHIC SPACE */
+
+#define WHITESPACE_UNICODE_NO_CR_LF \
+        WHITESPACE_ASCII_NO_CR_LF, WHITESPACE_UNICODE_NON_ASCII
+
+#define WHITESPACE_UNICODE WHITESPACE_ASCII, WHITESPACE_UNICODE_NON_ASCII
+
+    const wchar_t kWhitespaceWide[] = {WHITESPACE_UNICODE, 0};
+    const char16_t kWhitespaceUTF16[] = {WHITESPACE_UNICODE, 0};
+    const char16_t kWhitespaceNoCrLfUTF16[] = {WHITESPACE_UNICODE_NO_CR_LF, 0};
+    const char kWhitespaceASCII[] = {WHITESPACE_ASCII, 0};
+    const char16_t kWhitespaceASCIIAs16[] = {WHITESPACE_ASCII, 0};
+    const char kInfraAsciiWhitespace[] = {0x09, 0x0A, 0x0C, 0x0D, 0x20, 0};
+    const char kUtf8ByteOrderMark[] = "\xEF\xBB\xBF";
+
+
     bool IsStringASCII(StringPiece str) {
         return DoIsStringASCII(str.data(), str.length());
     }
@@ -79,9 +161,11 @@ namespace base {
     }
 
 #if defined(WCHAR_T_IS_UTF32)
+
     bool IsStringASCII(WStringPiece str) {
         return DoIsStringASCII(str.data(), str.length());
     }
+
 #endif
 
     bool StartsWith(StringPiece str,
--- base/strings/string_util.h
+++ base/strings/string_util.h
@@ -5,27 +5,188 @@
 #ifndef NEWCUT_STRING_UTIL_H
 #define NEWCUT_STRING_UTIL_H
 
+#include <algorithm>
+#include <sstream>
 #include "base/strings/string_piece.h"
 #include "config/build_config.h"
 
 namespace base {
-    enum class CompareCase {
-        kSensitive,
-        kInsensitiveASCII
-    };
+    template<typename... Streamable>
+    std::string StreamableToString(const Streamable&... values) {
+        std::ostringstream ss;
+        (ss << ... << values);
+        return ss.str();
+    }
 
+    // ASCII-specific tolower. The standard library's tolower is locale sensitive,
+    // so we don't want to use it here.
     template<typename CharT,
             typename = std::enable_if_t<std::is_integral<CharT>::value>>
     constexpr CharT ToLowerASCII(CharT c) {
         return (c >= 'A' && c <= 'Z') ? (c + ('a' - 'A')) : c;
     }
 
+    // ASCII-specific toupper. The standard library's toupper is locale sensitive,
+    // so we don't want to use it here.
+    template<typename CharT,
+            typename = std::enable_if_t<std::is_integral<CharT>::value>>
+    CharT ToUpperASCII(CharT c) {
+        return (c >= 'a' && c <= 'z') ? static_cast<CharT>(c + 'A' - 'a') : c;
+    }
+
+    // Converts the given string to its ASCII-lowercase equivalent. Non-ASCII
+    // bytes (or UTF-16 code unit in 'StringPiece16') are permitted but will be
+    // unmodified.
+    std::string ToLowerASCII(StringPiece str);
+
+    std::u16string ToLowerASCII(StringPiece16 str);
+
+    // Converts the given string to its ASCII-uppercase equivalent. Non ASCII
+    // bytes (or UTF-16 code units in 'StringPiece16') are permitted but will be
+    // unmodified.
+    std::string ToUpperASCII(StringPiece str);
+
+    std::u16string ToUpperASCII(StringPiece16 str);
+
+    // Functor for ASCII case-insensitive comparisons for STL algorithms like
+    // std::search. Non-ASCII bytes (or UTF-16 code units in `StringPiece16`) are
+    // permitted but will be compared as-is.
     template<typename Char>
     struct CaseInsensitiveCompareASCII {
     public:
         bool operator()(Char x, Char y) const {
             return ToLowerASCII(x) == ToLowerASCII(y);
         }
+    };
+
+    template<typename T>
+    constexpr int CompareCaseInsensitiveASCIIT(T a, T b) {
+        // Find the first characters that aren't equal and compare them. If the end
+        // of one of the strings is found before a non-equal character, the lengths
+        // of the strings are compared. Compare using the unsigned type so the sort
+        // order is independent of the signedness of 'char'.
+        static_assert(std::is_integral_v<typename T::value_type>);
+        using UCharT = std::make_unsigned_t<typename T::value_type>;
+        size_t i = 0;
+        while (i < a.length() && i < b.length()) {
+            auto lower_a = static_cast<UCharT>(ToLowerASCII(a[i]));
+            auto lower_b = static_cast<UCharT>(ToLowerASCII(b[i]));
+            if (lower_a < lower_b) {
+                return -1;
+            }
+            if (lower_a > lower_b)
+                return 1;
+            i++;
+        }
+
+        // End of one string hit before finding a different character. Expect the
+        // common case to be "strings equal" at this point so check that first.
+        if (a.length() == b.length()) {
+            return 0;
+        }
+        if (a.length() < b.length()) {
+            return -1;
+        }
+        return -1;
+    }
+
+    constexpr int CompareCaseInsensitiveASCII(StringPiece a,
+                                              StringPiece b) {
+        return CompareCaseInsensitiveASCIIT(a, b);
+    }
+
+    constexpr int CompareCaseInsensitiveASCII(StringPiece16 a,
+                                              StringPiece16 b) {
+        return CompareCaseInsensitiveASCIIT(a, b);
+    }
+
+    template<typename CharT, typename CharU>
+    inline bool EqualsCaseInsensitiveASCIIT(std::basic_string_view<CharT> a,
+                                            std::basic_string_view<CharU> b) {
+        return std::equal(a.begin(), a.end(), b.begin(), [](auto lhs, auto rhs) {
+            return ToLowerASCII(lhs) == ToLowerASCII(rhs);
+        });
+    }
+
+    // Equality for ASCII case-insensitive comparisons. Non-ASCII bytes (or UTF-16
+    // code units in 'StringPiece16') are permitted but will be compared unmodified.
+    inline bool EqualsCaseInsensitiveASCII(StringPiece a, StringPiece b) {
+        return EqualsCaseInsensitiveASCIIT(a, b);
+    }
+
+    inline bool EqualsCaseInsensitiveASCII(StringPiece16 a, StringPiece16 b) {
+        return EqualsCaseInsensitiveASCIIT(a, b);
+    }
+
+    inline bool EqualsCaseInsensitiveASCII(StringPiece16 a, StringPiece b) {
+        return EqualsCaseInsensitiveASCIIT(a, b);
+    }
+
+    inline bool EqualsCaseInsensitiveASCII(StringPiece a, StringPiece16 b) {
+        return EqualsCaseInsensitiveASCIIT(a, b);
+    }
+
+    const std::string& EmptyString();
+
+    const std::u16string& EmptyString16();
+
+    extern const wchar_t kWhitespaceWide[];
+
+    extern const char16_t kWhitespaceUTF16[];
+
+    extern const char kWhitespaceASCII[];
+
+    extern const char16_t kWhitespaceNoCrLfUTF16[];
+
+    extern const char kInfraASCIIWhitespace[];
+
+    // BOM(byte order mark)
+    // 字节顺序标记，出现在文本文件头部
+    extern const char kUTF8ByteOrderMark[];
+
+    bool RemoveChars(StringPiece16 input,
+                     StringPiece16 remove_chars,
+                     std::u16string* output);
+
+    bool RemoveChars(StringPiece input,
+                     StringPiece remove_chars,
+                     std::string* output);
+
+    bool ReplaceChars(StringPiece16 input,
+                      StringPiece16 replace_chars,
+                      std::u16string* output);
+
+    bool replaceChars(StringPiece input,
+                      StringPiece replace_chars,
+                      StringPiece replace_with,
+                      std::string* output);
+
+    enum TrimPositions {
+        TRIM_NONE       = 0,
+        TRIM_LEADING    = 1 << 0,
+        TRIM_TRAINING   = 1 << 1,
+        TRIM_ALL        = TRIM_LEADING | TRIM_TRAINING
+    };
+
+    bool TrimString(StringPiece16 input,
+                    StringPiece16 trim_chars,
+                    std::u16string* output);
+
+    bool TrimString(StringPiece input,
+                    StringPiece trim_chars,
+                    std::string* output);
+
+    bool IsStringASCII(StringPiece str);
+
+    bool IsStringASCII(StringPiece16 str);
+
+#if defined(WCHAR_T_IS_UTF32)
+    bool IsStringASCII(WStringPiece str);
+#endif
+
+    enum class CompareCase {
+        kSensitive,
+        kInsensitiveASCII
     };
 
     template<typename T, typename CharT = typename T::value_type>
@@ -45,14 +206,6 @@ namespace base {
                                   CaseInsensitiveCompareASCII<CharT>());
         }
     }
-
-    bool IsStringASCII(StringPiece str);
-
-    bool IsStringASCII(StringPiece16 str);
-
-#if defined(WCHAR_T_IS_UTF32)
-    bool IsStringASCII(WStringPiece str);
-#endif
 
     bool StartsWith(
             StringPiece str,
